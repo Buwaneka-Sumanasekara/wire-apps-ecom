fbe027a91adb83ee7c70382df5f5f5267379534a
Buwaneka Sumanasekara <buwanekasumanasekara@gmail.com>
2024-05-07 20:42:51 +0530

    fixed bug

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,16 +1,16 @@
 import { configureStore,applyMiddleware } from '@reduxjs/toolkit'
 import cartReducer from './modules/cartSlice'
 import productsReducer from './modules/productsSlice'
-import {productsApi} from '../services/productsApi'
+import {productsApiSlice} from '../services/productsApi'
 
 
 export const store = configureStore({
   reducer: {
     cart: cartReducer,
     products: productsReducer,
-    [productsApi.reducerPath]: productsApi.reducer,
+    [productsApiSlice.reducerPath]: productsApiSlice.reducer,
   },
   middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware().concat(productsApi.middleware),
+    getDefaultMiddleware().concat(productsApiSlice.middleware),
   devTools: process.env.NODE_ENV !== 'production',
 })
