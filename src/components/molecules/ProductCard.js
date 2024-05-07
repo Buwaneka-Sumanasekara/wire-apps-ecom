72eaa0eda921a953c11afc2f7e67db38b216292b
Buwaneka Sumanasekara <buwanekasumanasekara@gmail.com>
2024-05-07 17:35:34 +0530

    small ui issue fixed

--- src/components/molecules/ProductCard.js
+++ src/components/molecules/ProductCard.js
@@ -4,13 +4,13 @@ import { Ionicons } from '@expo/vector-icons';
 
 const ProductCard = ({ item }) => {
 
-    return <View className={"flex-1 px-1 pb-2"}>
-        <View className={"bg-white rounded-md justify-content-center p-3"}>
+    return <View className={"w-1/2 px-1 pb-2"}>
+        <View className={"flex-1  bg-white rounded-md justify-content-center p-3"}>
             <Image source={{ uri: item.mainImage }} className={"w-3/4 aspect-square self-center"} />
             <Text className={"text-base text-black font-semibold"}>{`${item.price.amount} ${item.price.currency}`}</Text>
-            <View className={"flex flex-row justify-between "}>
-                <Text ellipsizeMode={"tail"} numberOfLines={2} className={"text-sm text-slate-500 basis-4/5"}>{item.name}</Text>
-                <Ionicons name="heart-outline" size={25}  />
+            <View className={"flex flex-row"}>
+                <Text ellipsizeMode={"tail"} numberOfLines={2} className={"text-sm text-slate-500 w-4/5"}>{item.name}</Text>
+                <Ionicons name="heart-outline" size={25} className={"self-end"}  />
             </View>
         </View>
     </View>;
